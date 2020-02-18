d27f592c2deaa3d648dc8723db179916dfdd9ac7
james171 <26070680+James171@users.noreply.github.com>
2020-02-17 18:03:04 -0600

    initial commit for simple recursive example printing range of numbers

---
+++ recursive.js
@@ -0,0 +1,26 @@
+x = prompt("Pick a starting number: ");
+
+y = prompt("Pick a ending number: ");
+
+while(x > y){
+y = prompt("Please pick a larger number: ");
+}
+
+function rangeOfNumbers(startNum, endNum) {
+  let n = endNum - startNum;
+  if(1 > n){
+    return [startNum];
+  }
+  else{
+   const countArray = rangeOfNumbers(startNum, endNum - 1);
+   countArray.push(endNum);
+   return countArray;
+  }
+    
+}
+  
+  
+answer = rangeOfNumbers(x, y);
+
+
+document.getElementById('output').innerHTML = answer;
